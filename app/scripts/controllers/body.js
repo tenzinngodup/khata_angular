ace77610ce4bf53d410ea0c251497e2c54bedfd7
Tenzin Ngodup <tngodup@tenzins-mbp.attlocal.net>
2015-08-18 19:44:14 -0700

    background change

--- app/scripts/controllers/body.js
+++ app/scripts/controllers/body.js
@@ -15,12 +15,11 @@ angular.module('khataAngularApp')
       'Karma'
     ];
 
-    $scope.image="../../images/light-image.jpg";
-  $scope.bodyStyle = {background: "url(" + $scope.image + ") no-repeat center center fixed",
-  "-webkit-background-size": "cover",
-  "-moz-background-size": "cover",
-  "-o-background-size": "cover",
-  "background-size": "cover"};
+  // $scope.bodyStyle = {background: "url(" + $scope.image + ") no-repeat center center fixed",
+  // "-webkit-background-size": "cover",
+  // "-moz-background-size": "cover",
+  // "-o-background-size": "cover",
+  // "background-size": "cover"};
 
 
   });
